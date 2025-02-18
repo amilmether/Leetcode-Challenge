78e83502cbd25768a1f149276348079a25fbd16b
Amil <amilmether37@gmail.com>
2025-02-18 21:54:28 +0530

    1929 Added

---
+++ 1929_Concatenation_of_Array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self,nums):
+        temp=nums
+        return nums+temp
+solution=Solution()   
+nums=[1,2,3]
+res=solution.getConcatenation(nums)
+print(res)
