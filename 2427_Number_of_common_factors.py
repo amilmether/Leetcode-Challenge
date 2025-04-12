ac8d810188523ca37485121dbd0459d54aa6d2da
Amil <amilmether37@gmail.com>
2025-04-12 20:09:48 +0530

    Added advanced version of 2427

--- 2427_Number_of_common_factors.py
+++ 2427_Number_of_common_factors.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def commonFactors(self, a,b):
+        while b != 0:
+            a,b=b,a%b
+        gcd = a
         cf =[] 
-        for i in range(1, max(a,b)+1):
-            if (a % i == 0 and b % i == 0):
+        for i in range(1, gcd +1):
+            if (a % i == 0):
                 cf.append(i)
         return cf
 
