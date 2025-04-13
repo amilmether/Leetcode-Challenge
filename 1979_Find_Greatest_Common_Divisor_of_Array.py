909be3554c59fcd65dfe06fd1d47772e8f5a9d20
Amil <amilmether37@gmail.com>
2025-04-13 09:19:40 +0530

    1979 Corrected for [2,2,7]  Advanced Version -updated

--- 1979_Find_Greatest_Common_Divisor_of_Array.py
+++ 1979_Find_Greatest_Common_Divisor_of_Array.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def findGCD(self, nums):
         small=large=nums[0]
-        
-        if nums[0] == nums[1]:
-            small=large=nums[0]
-            return nums[0]
-        else:
-            for num in nums:
-                if num < small:
-                    small = num
-                if num > large:
-                    large = num
+        for num in nums:
+            if num < small:
+                small = num
+            if num > large:
+                large = num
+        if small == large :
+            return small
         while large!= 0:
             small,large=large,small%large 
         a=small
