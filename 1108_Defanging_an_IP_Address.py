8bacb67700881d97fe1f46ce53f4cc56334e091f
Amil <amilmether37@gmail.com>
2025-04-14 09:32:16 +0530

    1108 Advanced Version

--- 1108_Defanging_an_IP_Address.py
+++ 1108_Defanging_an_IP_Address.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def defangIPaddr(self, address):
-        array=[]
-        for i in address:
-            if i == '.':
-                array.append("[.]")
-            else:
-                array.append(i)
-        ip_address = ''.join(array)
-        return ip_address
+        return address.replace('.','[.]')
 
 sol=Solution()
 print(sol.defangIPaddr("123.45.67.89"))
