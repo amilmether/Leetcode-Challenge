8b9506fed023ca84adde4aebee98681c7d8f508d
Amil <amilmether37@gmail.com>
2025-04-15 10:30:05 +0530

    1 Advanced Version using hashmap

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        flag=0
-        pos=[]
-        for i in range(0,len(nums)):
-            for j in range(1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    pos.append(i)
-                    pos.append(j)
-                    flag=1
-                    break
-            if flag == 1:
-                return pos
+        hashmap={}
+        for i,n in enumerate(nums):
+            diff= target - n
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            hashmap[n] = i
+        return
         
 sol=Solution()
 arry=[10, 22, 5, 75, 65, 80, 30, 110, 90, 150, 120, 50, 70, 40]
