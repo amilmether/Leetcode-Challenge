33fe496f781f7301ca47f660a1e9acf5c6f185aa
Amil <amilmether37@gmail.com>
2025-04-16 09:45:52 +0530

    2974 Advanced Version  with O(nlogn) Used GPT

--- 2974_Minimum_Number_Game.py
+++ 2974_Minimum_Number_Game.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def numberGame(self, nums):
-        new = []
-        while nums:
-            alice = min(nums)
-            nums.remove(alice)
-            bob = min(nums)
-            nums.remove(bob)
-            new.append(bob)
-            new.append(alice)
+        nums.sort()
+        new=[]
+        for i in range(0,len(nums),2):#2 is the step up value like i+2
+            new.append(nums[i+1])
+            new.append(nums[i])
         return new
-        
 sol=Solution()
 nums=[5,4,2,3]
 print(sol.numberGame(nums))
