05c2911632a3b2ce5445d8390859b8bb0c389b7c
Amil <amilmether37@gmail.com>
2025-04-16 09:52:39 +0530

    2974 Advanced Version  with O(nlogn) Top in LeetCode

--- 2974_Minimum_Number_Game.py
+++ 2974_Minimum_Number_Game.py
@@ -2,9 +2,9 @@ class Solution(object):
     def numberGame(self, nums):
         nums.sort()
         new=[]
-        for i in range(0,len(nums),2):#2 is the step up value like i+2
-            new.append(nums[i+1])
-            new.append(nums[i])
+        for a,b in zip(nums[::2],nums[1::2]):#2 is the step up value like i+2
+            new.append(b)
+            new.append(a)
         return new
 sol=Solution()
 nums=[5,4,2,3]
