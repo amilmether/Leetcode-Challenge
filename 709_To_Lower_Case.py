a48480d7e75d43e85ebdeccd186b9633f1af0e6c
Amil <amilmether37@gmail.com>
2025-04-17 09:49:02 +0530

    709 Advanced Version added O(n)

--- 709_To_Lower_Case.py
+++ 709_To_Lower_Case.py
@@ -1,6 +1,11 @@
 class Solution(object):
     def toLowerCase(self, s):
-        final=s.lower()
-        return final
+        final=[]
+        for c in s:
+            if 'A' <= c <= 'Z':
+                final.append(chr(ord(c) + 32))
+            else:
+                final.append(c)
+        return ''.join(final)
 sol=Solution()
 print(sol.toLowerCase("HELLO"))
