cb182ab66d18c1d663b4ffa0d0a1ac29bc6d150a
Amil <amilmether37@gmail.com>
2025-04-18 09:12:25 +0530

    657 Normal Version Added with O(n)

---
+++ 657_Robot_Return_to_Origin.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        x=y=0
+        for i in moves:
+            if i == 'U':
+                y=y+1
+            elif i == 'D':
+                y=y-1
+            elif i == 'L':
+                x=x-1
+            elif i == 'R':
+                x=x+1
+        if x == 0 and y == 0:
+            return True
+        else:
+            return False
+
+
+sol=Solution()
+print(sol.judgeCircle("UD"))
