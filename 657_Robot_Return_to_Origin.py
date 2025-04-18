855247c8587d13df75c8388e8edce7a5003b866a
Amil <amilmether37@gmail.com>
2025-04-18 09:28:33 +0530

    657 Advanced Version Added same  O(n) with Counter

--- 657_Robot_Return_to_Origin.py
+++ 657_Robot_Return_to_Origin.py
@@ -1,19 +1,8 @@
+from collections import Counter
 class Solution(object):
     def judgeCircle(self, moves):
-        x=y=0
-        for i in moves:
-            if i == 'U':
-                y=y+1
-            elif i == 'D':
-                y=y-1
-            elif i == 'L':
-                x=x-1
-            elif i == 'R':
-                x=x+1
-        if x == 0 and y == 0:
-            return True
-        else:
-            return False
+        c = Counter(moves)
+        return c['U'] == c['D'] and c['L'] == c['R']
 
 
 sol=Solution()
