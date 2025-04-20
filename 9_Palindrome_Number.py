d1b99a1952af22b429760a8886bccc8ca15c579a
Amil <amilmether37@gmail.com>
2025-04-20 20:15:00 +0530

    9 Advanced Version added lazy code

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -1,15 +1,6 @@
 class Solution(object):
     def isPalindrome(self, x):
-       temp=x
-       rev=0
-       while x>0:
-           rev=rev * 10 + x%10
-           x=x//10
-       if rev == temp:
-            return True
-       else:
-            return False
-
+        return str(x) == str(x)[::-1]
 
 sol=Solution()
 print(sol.isPalindrome(121))
