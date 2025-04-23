f6e2b9b0009b0104b0c99119e5f0a85069c65c9c
Amil <amilmether37@gmail.com>
2025-04-23 11:19:41 +0530

    2180 Advanced  version Added --updated

--- 2180_Count_Integers_With_Even_Digit_Sum.py
+++ 2180_Count_Integers_With_Even_Digit_Sum.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def countEven(self, num):
-        def countsum(n):
-            return sum(map(int , str(n)))
-        return sum(1 for i in range(1,num+1) if countsum(i)%2 == 0)
+        digit_sum=sum(int(d) for d in str(num))
+        return num // 2 if digit_sum % 2 == 0 else (num-1) // 2
 sol=Solution()
 print(sol.countEven(30))
