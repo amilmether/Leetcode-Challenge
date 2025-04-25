0ca355db255391f0d7465fcaaf7b001741fbcf4e
Amil <amilmether37@gmail.com>
2025-04-25 10:32:38 +0530

    507 Advanced Version Added ---updated Pro code

--- 507_Perfect_Number.py
+++ 507_Perfect_Number.py
@@ -1,14 +1,7 @@
 class Solution(object):
     def checkPerfectNumber(self, num):
-        if num<=1:
-            return False
-        sum_divisors=1
-        for i in range(2,int(num**0.5)+ 1):
-            if num % i == 0:
-                sum_divisors+=i
-                if i != num // i:
-                    sum_divisors += num // i
-        return sum_divisors == num
+        perfect_num=[6,28,496,8128,33550336]
+        return num in perfect_num
     
 sol= Solution()
 print(sol.checkPerfectNumber(28))
