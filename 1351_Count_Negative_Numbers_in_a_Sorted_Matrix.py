c2d77369e35e609a70e06a5b51feaa30f60480fc
Amil <amilmether37@gmail.com>
2025-05-05 22:28:47 +0530

    1351 Advanced Version Added

--- 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -1,13 +1,13 @@
+import itertools
 class Solution(object):
     def countNegatives(self, grid):
         count=0
-        for i in grid:
-            for j in i:
-                if j<0:
-                    count+=1
+        for item in itertools.chain.from_iterable(grid):
+            if item < 0:
+                count+=1
         return count
     
 sol=Solution()
-arr=[[3,2],[1,0]]
+arr=[[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 print(sol.countNegatives(arr))
         
