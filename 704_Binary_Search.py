4de55328c50f10933fb492e053b83888efe007f7
Amil <amilmether37@gmail.com>
2025-05-10 19:45:50 +0530

    added sol print

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def search(self, nums, target):
         left, right = 0, len(nums) - 1
-
         while left <= right:
             mid = (left + right) // 2
 
@@ -12,7 +11,7 @@ class Solution(object):
             else:
                 right = mid - 1
 
-        return -1
+        return mid+1
 sol=Solution()
 arr=[-1,0,3,5,9,12]
-print(sol.search(arr,9))
+print(sol.search(arr,5))
