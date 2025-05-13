afaf581c592acd54b3579fce8530143468d54de8
Amil <amilmether37@gmail.com>
2025-05-13 09:20:10 +0530

    268 Acvanced  version added

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def missingNumber(self, nums):
-        for i in range(0,len(nums)+1):
-            if i in nums:
-                pass
-            else:
-                return i
+        return sum(range(0,len(nums)+1))-sum(nums)
 
 sol=Solution()
-arr=[9,6,4,2,3,5,7,0,1]
+arr=[0,1]
 print(sol.missingNumber(arr))
