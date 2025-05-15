729214922fc706e080f6eace629718228219a58d
Amil <amilmether37@gmail.com>
2025-05-15 11:30:28 +0530

    1773 normal with elif

--- 1773_Count_Items_Matching_a_Rule.py
+++ 1773_Count_Items_Matching_a_Rule.py
@@ -1,22 +1,21 @@
 class Solution(object):
     def countMatches(self, items, ruleKey, ruleValue):
         count=0
-        match(ruleKey):
-            case "color":
-                for t,c,n in items:
-                    if c == ruleValue:
-                        count+=1
-                return count
-            case "type":
-                for t,c,n in items:
-                    if t == ruleValue:
-                        count+=1
-                return count
-            case "name":
-                for t,c,n in items:
-                    if n == ruleValue:
-                        count+=1
-                return count
+        if ruleKey == "color":
+            for t,c,n in items:
+                if c == ruleValue:
+                    count+=1
+            return count
+        elif ruleKey == "type":
+            for t,c,n in items:
+                if t == ruleValue:
+                    count+=1
+            return count
+        elif ruleKey== "name":
+            for t,c,n in items:
+                if n == ruleValue:
+                    count+=1
+            return count
         
 
 sol=Solution()
