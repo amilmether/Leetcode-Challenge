2e60f0a1df093270b335c49f52aa3466a8860505
Amil <amilmether37@gmail.com>
2025-05-21 19:37:05 +0530

    66 Advanced

--- 66_Plus_One.py
+++ 66_Plus_One.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def plusOne(self, digits):
-        stri=''.join(map(str,digits))
-        new= int(stri)+1
-        return [int(d) for d in str(new)]
+        for i in reversed(range(len(digits))):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i]=0
+        return [1] + digits
 sol=Solution()
 arr=[1,2,3]
 print(sol.plusOne(arr))
