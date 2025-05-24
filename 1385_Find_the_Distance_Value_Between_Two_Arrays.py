b600b940d1d55b5981b9b0b018891db7df3f5c38
Amil <amilmether37@gmail.com>
2025-05-24 23:38:45 +0530

    1385 Advanced

--- 1385_Find_the_Distance_Value_Between_Two_Arrays.py
+++ 1385_Find_the_Distance_Value_Between_Two_Arrays.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def findTheDistanceValue(self, arr1, arr2, d):
+        arr1.sort()
+        arr2.sort()
         count=0
-        for num1 in arr1:
-            is_far=True
-            for num2 in arr2:
-                if abs(num1-num2) <=d:
-                    is_far=False
-                    break
-            if is_far:
-                count+=1
+        j=0
+        for i in range(len(arr1)):
+            while j < len(arr2) and arr2[j] < arr1[i]-d:
+                j+=1
+            if j== len(arr2) or arr2[j] > arr1[i]+d:
+                count +=1
         return count
-
 sol=Solution()
 arr1=[4,5,8]
 arr2=[10,9,1,8]
