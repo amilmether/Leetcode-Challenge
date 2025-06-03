7492df52e4635f56f05d8d6483c0cb578a48a4b5
Amil <amilmether37@gmail.com>
2025-06-03 20:43:46 +0530

    389 Advanced

--- 389_Find_the_Difference.py
+++ 389_Find_the_Difference.py
@@ -1,14 +1,16 @@
-from typing import Counter
+
 
 
 class Solution(object):
-    def findTheDifference(self, s, t):
-        count_s, count_t = Counter(s), Counter(t)
-        for c in count_t:
-            if c not in count_s:
-                return c
-            if count_s[c] < count_t[c]:
-                return c
+    def findTheDifference(self, s: str, t: str)  -> str:
+        sum_s, sum_t = 0,0
+        for c in s:
+            sum_s += ord(c)
+        for c in t:
+            sum_t += ord(c)
+            
+        return chr(sum_t - sum_s)
+        
 
 
 sol = Solution()
