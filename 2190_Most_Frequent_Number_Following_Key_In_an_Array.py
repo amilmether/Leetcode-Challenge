ed66d1b8e5681493059d3ef4229407d20d2c5832
Amil <amilmether37@gmail.com>
2025-06-13 00:55:28 +0530

    2190 added

---
+++ 2190_Most_Frequent_Number_Following_Key_In_an_Array.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution(object):
+    def mostFrequent(self,nums, key):
+        count = defaultdict(int)
+        for i in range(len(nums)-1):
+            if nums[i] == key:
+                count[nums[i+1]] += 1
+        return max(count,key=count.get)
