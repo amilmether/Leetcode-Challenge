f275d91d0b59a135170a86693e715bf962c38519
Amil <amilmether37@gmail.com>
2025-06-23 18:58:14 +0530

    442 added

--- ProblemNameFixer.py
+++ ProblemNameFixer.py
@@ -9,4 +9,4 @@ def fixer(text):
     print(f"{filename} created successfully!")
 
 # Example usage
-fixer("1689. Partitioning Into Minimum Number Of Deci-Binary Numbers")
+fixer("442. Find All Duplicates in an Array")
